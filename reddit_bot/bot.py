6470acecfc3c55ab7f4d1426207ab460cb7ce02f
Kris Bredemeier <34@holbertonschool.com>
2016-09-23 14:08:02 -0700

    adds send email

--- reddit_bot/bot.py
+++ reddit_bot/bot.py
@@ -1,30 +1,57 @@
 import praw
 import time
+import smtplib
 
-r = praw.Reddit(user_agent= "holberton keyword search bot by kris bredemeier")
-r.login()
 
-words_to_match = ['yo', 'tap']
+#import creds
+USERAGENT = 'holberton keyword search by /u/school_bot'
+USERNAME = 'school_bot'
+PASSWORD = 'holberton'
+
+SUBREDDIT_TITLE = "test"
+MAXPOSTS = 10
+REPLY = 'truth'
+
+SENDER_EMAIL = 'myschoolbot@gmail.com'
+RECIPIENT_EMAIL = ['kris.bredemeier@holbertonschool.com']
+
+
+r = praw.Reddit(user_agent= USERAGENT)
+r.login(USERNAME, PASSWORD)
+
+words_to_match = ['getting better at python programming']
 cache = []
 
 def run_bot():
     print("Grabbing subreddit...")
-    subreddit = r.get_subreddit("funny")
+    subreddit = r.get_subreddit(SUBREDDIT_TITLE)
     print("Grabbing comments...")
-    comments = subreddit.get_comments(limit=25)
+    comments = subreddit.get_comments(limit= MAXPOSTS)
+    # message = subreddit.get_message(limit= MAXPOSTS)
     for comment in comments:
         comment_text = comment.body.lower()
         isMatch = any(string in comment_text for string in words_to_match)
         if comment.id not in cache and isMatch:
             print("Match found! comment ID: " + comment.id)
-            comment.reply('test')
+            comment.reply(REPLY)
             print("Reply successful!")
             cache.append(comment.id)
     print("Comments loop finished, time to sleep")
 
+def send_email():
+    content = 'test' #url of reddit post that has our keyword in it
+    mail = smtplib.SMTP('smtp.gmail.com', 587)
+    mail.ehlo()
+    mail.starttls()
+    mail.login(SENDER_EMAIL, PASSWORD)
+    mail.sendmail(SENDER_EMAIL, RECIPIENT_EMAIL, content)
+    mail.close
+
 while True:
     run_bot()
+    send_email()
     time.sleep(10)
 
-
-# [Holberton School](https://www.holbertonschool.com)
+# KEYWORDS ['CS']
+# SUBREDDITS ['technology', 'software engineering', 'career questions']
+# FLAIR ['software']
